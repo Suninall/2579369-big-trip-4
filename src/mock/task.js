a1a31cd0bee8ed8ff8a48fb496df0d02a1ac6b99
Suninall <sanina-nsk@mail.ru>
2025-02-10 21:46:41 +0700

    задание 2.1

---
+++ src/mock/task.js
@@ -0,0 +1,32 @@
+const mockTask = [
+  {
+    type:'Flight',
+    destination:'Geneva',
+    dateStart:new Date('2025-06-03'),
+    dateEnd:new Date('2025-14-03'),
+    cost:300,
+    offers:{
+      addLunguauge:true,
+      switchToComfortClass:true,
+      addMeal:false,
+      chooseSeats:false,
+      travelByTrain:false,
+    },
+
+  },
+  {
+    type:'Car',
+    destination:'Geneva',
+    dateStart:new Date('2025-06-03'),
+    dateEnd:new Date('2025-14-03'),
+    cost:300,
+    offers:{
+      addLunguauge:true,
+      switchToComfortClass:true,
+      addMeal:false,
+      chooseSeats:false,
+      travelByTrain:false,
+    },
+
+  },
+];
